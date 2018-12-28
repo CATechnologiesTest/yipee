1b980b227e0f7aee264490a88d6c04c1d9a35780
Mark Emeis <mark.emeis@ca.com>
2018-12-28 13:59:02 -0700

    Add container-name to volume-ref flat file generation.

--- ui/src/app/models/common/VolumeRef.spec.ts
+++ ui/src/app/models/common/VolumeRef.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 import { VolumeRef } from './VolumeRef';
-import { NameChangeEvent } from '../Events';
+import { Container } from './Container';
+import { Finder } from '../parse/Finder';
+
 
 describe('VolumeRef', () => {
-
+  const roundTripID = '12s';
+  const roundTripName = 'foo';
   const flat1 = {
     'type': 'volume-ref',
-    'container': 'd0bef4cf-5f0b-4fa1-b8b0-5da4bfe6b26c',
+    'container': roundTripID,
+    'container-name': roundTripName,
     'volume': 'd0bef4cf-5f0b-4fa1-b8b0-dddddddddd',
     'volume-name': 'ERROR',
     'path': '/var/log',
@@ -16,9 +20,36 @@ describe('VolumeRef', () => {
   };
 
   it('should handle round trip', () => {
+    const f = new Finder();
+    const c1 = Container.construct(Container.OBJECT_NAME) as Container;
+    c1.id = roundTripID;
+    c1.name = roundTripName;
+    f.push(c1);
     const a1 = VolumeRef.construct(VolumeRef.OBJECT_NAME);
+    f.push(a1);
     a1.fromFlat(flat1);
     expect(a1.toFlat()).toEqual(flat1);
+  });
+
+  it('should add the container name to the volumeref', () => {
+    const f = new Finder();
+    const v1 = VolumeRef.construct(VolumeRef.OBJECT_NAME) as VolumeRef;
+    const c1 = Container.construct(Container.OBJECT_NAME) as Container;
+    f.push(c1);
+    f.push(v1);
+    expect(v1.finder).toBeDefined();
+    expect(c1.finder).toEqual(v1.finder);
+    c1.name = 'foo';
+    v1.container = c1.id;
+    let flat = v1.toFlat();
+    expect(flat['container-name']).toBeDefined();
+    expect(flat['container-name']).toEqual('foo');
+    // simulate a name change to the container
+    c1.name = 'foo2';
+    flat = v1.toFlat();
+    expect(flat['container-name']).toBeDefined();
+    expect(flat['container-name']).toEqual('foo2');
+
   });
 
 });
--- ui/src/app/models/common/VolumeRef.ts
+++ ui/src/app/models/common/VolumeRef.ts
@@ -2,6 +2,8 @@ import { ParsedObject } from '../parse/ParsedObject';
 import { Annotation } from './Annotation';
 import { BaseVolume } from './BaseVolume';
 import { Volume } from './Volume';
+import { Container } from './Container';
+
 
 /** common volume-ref entry */
 
@@ -41,10 +43,27 @@ export class VolumeRef extends ParsedObject {
     flat['path'] = this.path;
     flat['sub-path'] = this.sub_path;
     flat['container'] = this.container;
+    // For proper validation, the backend requires the container name to be passed
+    flat['container-name'] = this.findContainerNameById(this.container);
     flat['volume'] = this.volume;
     flat['access-mode'] = this.access_mode;
     flat['volume-name'] = this.volume_name;
     return flat;
+  }
+
+  findContainerNameById(id: string): string {
+    if (this.finder === undefined) {
+      return undefined;
+    }
+    const c = this.finder.objects
+      .filter((p) => p.type === Container.OBJECT_NAME)
+      .map((p: Container) => p as Container)
+      .find((p) => p.id === id);
+
+    if (c) {
+      return c.name;
+    }
+    return undefined;
   }
 
   get volume_name(): string {
