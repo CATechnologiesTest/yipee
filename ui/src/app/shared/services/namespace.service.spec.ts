3f79270f5da23e7ea3714e1c8d521465517926b8
Brett Bloethner <Brett.Bloethner@broadcom.com>
2019-01-09 13:53:33 -0700

    add testing module to the namespace service test

--- ui/src/app/shared/services/namespace.service.spec.ts
+++ ui/src/app/shared/services/namespace.service.spec.ts
@@ -1,9 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 
 import { NamespaceService } from './namespace.service';
+import { ApiService } from './api.service';
+
+class MockApiService {
+  constructor() { }
+}
 
 describe('NamespaceService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      { provide: ApiService, useClass: MockApiService },
+    ]
+  }));
 
   it('should be created', () => {
     const service: NamespaceService = TestBed.get(NamespaceService);
