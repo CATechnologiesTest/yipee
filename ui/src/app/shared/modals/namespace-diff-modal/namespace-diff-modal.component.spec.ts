365bdd7e54db758acae3de008e6bf32c7f97a7f9
AJ Copley <andrew.copley@ca.com>
2019-01-15 11:52:03 -0700

    Implemented fix for diffing two yamls and wrote tests for diff modal.

--- ui/src/app/shared/modals/namespace-diff-modal/namespace-diff-modal.component.spec.ts
+++ ui/src/app/shared/modals/namespace-diff-modal/namespace-diff-modal.component.spec.ts
@@ -1,19 +1,17 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientModule, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { NamespaceDiffModalComponent } from './namespace-diff-modal.component';
-import { MarkdownService } from 'ngx-markdown';
-import { NamespaceRaw } from '../../../models/YipeeFileRaw';
 import { FormBuilder } from '@angular/forms';
 import { ApiService } from '../../services/api.service';
 import { NamespaceService } from '../../services/namespace.service';
 
-class MockApiService {
-  constructor() { }
-}
-
 class MockNamespaceService {
   constructor() { }
+  currentNamespaces = [];
 }
 
 describe('NamespaceDiffModalComponent', () => {
@@ -24,10 +22,15 @@ describe('NamespaceDiffModalComponent', () => {
     TestBed.configureTestingModule({
       declarations: [NamespaceDiffModalComponent],
       schemas: [NO_ERRORS_SCHEMA],
+      imports: [
+        HttpClientModule,
+        HttpClientTestingModule,
+        RouterTestingModule
+      ],
       providers: [
-        { provide: FormBuilder, useClass: FormBuilder },
-        { provide: NamespaceService, useClass: MockNamespaceService },
-        { provide: ApiService, useClass: MockApiService },
+        ApiService,
+        FormBuilder,
+        { provide: NamespaceService, useClass: MockNamespaceService }
       ]
     }).compileComponents();
   }));
@@ -42,5 +45,62 @@ describe('NamespaceDiffModalComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should return a successful diff', async(inject([HttpTestingController], (backend: HttpTestingController) => {
+    component.parentNamespace = 'foo';
+    component.childNamespace = 'bar';
+    component.compare();
+    backend.expectOne('/api/namespaces/diff').flush({
+      success: true,
+      total: 1,
+      data: ['foo, bar']
+    });
+    expect(component.diffResults).toEqual('foo, bar');
+  })));
+
+  it('should return an unsuccessful diff', async(inject([HttpTestingController], (backend: HttpTestingController) => {
+    component.parentNamespace = 'foo';
+    component.diffForm.controls.childNamespace.setValue('bar');
+    component.diffForm.controls.childNamespace.markAsDirty();
+    const err = 'Bad yaml';
+    component.compare();
+    backend.expectOne({ method: 'POST', url: '/api/namespaces/diff' })
+      .flush({
+        success: false,
+        total: 0,
+        data: [err]
+      },
+        {
+          status: 200, statusText: 'badyaml'
+        });
+    expect(component.diffResults).toEqual(NamespaceDiffModalComponent.ERROR_MSG + err);
+  })));
+
+  it('should handle a network error', async(inject([HttpTestingController], (backend: HttpTestingController) => {
+    component.parentNamespace = 'foo';
+    component.childNamespace = null;
+    const err = 'bad child namespace';
+    component.compare();
+    backend.expectOne({ method: 'POST', url: '/api/namespaces/diff' })
+      .flush({
+        success: false,
+        total: 0,
+        data: [err]
+      },
+        {
+          status: 500, statusText: 'badDev'
+        });
+    expect(component.diffResults).toEqual(NamespaceDiffModalComponent.ERROR_MSG + err);
+
+  })));
+
+  it('should emit a string of closeNamespaceDiff, clear diffResults, and set showDiffResults to false, when component close() function is called', () => {
+    component.onClose.subscribe((value: string) => {
+      expect(component.diffResults).toEqual('');
+      expect(component.showDiffResults).toBeFalsy();
+      expect(value).toBeDefined();
+      expect(value).toEqual('closeNamespaceDiff');
+    });
+    component.close();
+  });
 
 });
